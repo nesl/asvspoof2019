c88c50b70917aed638cecf694f56b03d9a65c4fb
Moustafa Alzantot <malzantot@ucla.edu>
2019-02-01 14:56:32 -0800

    fix bug

--- download_data.bash
+++ download_data.bash
@@ -11,7 +11,7 @@ test -f ASVspoof2019_LA_dev_v1.tar && (tar -xvf ASVspoof2019_LA_dev_v1.tar     &
 test -f ASVspoof2019_LA_protocols_v1.tar || wget -c ftp://ftp.asvspoof.org/incoming/ASVspoof2019_LA_protocols_v1.tar
 test -f ASVspoof2019_LA_protocols_v1.tar && (tar -xvf ASVspoof2019_LA_protocols_v1.tar && rm -f ASVspoof2019_LA_protocols_v1.tar)
 test -f ASVspoof2019_LA_dev_asv_scores_v1.txt || wget -c ftp://ftp.asvspoof.org/incoming/ASVspoof2019_LA_dev_asv_scores_v1.txt
-test -f ASVspoof2019_LA_dev_asv_scores_v1.txt  || wget -c ftp://ftp.asvspoof.org/incoming/ASVspoof2019_LA_instructions_v1.txt
+test -f  ASVspoof2019_LA_instructions_v1.txt  || wget -c ftp://ftp.asvspoof.org/incoming/ASVspoof2019_LA_instructions_v1.txt
 
 cd ../
 echo 'Downloading physical access dataset'
@@ -21,7 +21,7 @@ cd data_physical
 test -f ASVspoof2019_PA_train_v1.tar  || wget -c ftp://ftp.asvspoof.org/incoming/ASVspoof2019_PA_train_v1.tar
 test -f ASVspoof2019_PA_train_v1.tar  && (tar -xvf ASVspoof2019_PA_train_v1.tar  && rm -f ASVspoof2019_PA_train_v1.tar )
 
-test -f ASVspoof2019_PA_train_v1.tar  || wget -c ftp://ftp.asvspoof.org/incoming/ASVspoof2019_PA_dev_v1.tar
+test -f ASVspoof2019_PA_dev_v1.tar  || wget -c ftp://ftp.asvspoof.org/incoming/ASVspoof2019_PA_dev_v1.tar
 test -f ASVspoof2019_PA_dev_v1.tar && (tar -xvf ASVspoof2019_PA_dev_v1.tar && rm -f ASVspoof2019_PA_dev_v1.tar)
 
 test -f ASVspoof2019_PA_protocols_v1.tar  || wget -c ftp://ftp.asvspoof.org/incoming/ASVspoof2019_PA_protocols_v1.tar
