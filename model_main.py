4ac13071a76ce2b78d32e597a19bd9d5287c9bc2
Moustafa Alzantot <malzantot@ucla.edu>
2019-02-18 22:03:14 -0800

    More options for model training with different features and with both tracks

--- model_main.py
+++ model_main.py
@@ -16,6 +16,7 @@ import torch
 from torch import nn 
 from tensorboardX import SummaryWriter
 
+from models import SpectrogramModel, MFCCModel
 from scipy.optimize import brentq
 from scipy.interpolate import interp1d
 from sklearn.metrics import roc_curve
@@ -29,84 +30,9 @@ def pad(x, max_len=64000):
     padded_x = x_repeat[:max_len]
     return padded_x
 
-class ResNetBlock(nn.Module):
-    def __init__(self, in_depth, depth, first=False):
-        super(ResNetBlock, self).__init__()
-        self.first = first
-        self.conv1 = nn.Conv2d(in_depth, depth, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(depth)
-        self.lrelu = nn.LeakyReLU(0.01)
-        self.dropout = nn.Dropout(0.5)
-        self.conv2 = nn.Conv2d(depth, depth, kernel_size=3, stride=1, padding=1)
-        self.conv11 = nn.Conv2d(in_depth, depth, kernel_size=3, stride=1, padding=1)
-        if not self.first :
-            self.pre_bn = nn.BatchNorm2d(in_depth)
 
-    def forward(self, x):
-        # x is (B x d_in x T)
-        prev = x
-        prev_mp =  self.conv11(x)
-        if not self.first:
-            out = self.pre_bn(x)
-            out = self.lrelu(out)
-        else:
-            out = x
-        out = self.conv1(x)
-        # out is (B x depth x T/2)
-        out = self.bn1(out)
-        out = self.lrelu(out)
-        out = self.dropout(out)
-        out = self.conv2(out)
-        # out is (B x depth x T/2)
-        out = out + prev_mp
-        return out
 
-class ConvModel(nn.Module):
-    def __init__(self):
-        super(ConvModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
-        self.block1 = ResNetBlock(32, 32,  True)
-        self.mp = nn.MaxPool2d(3, stride=3, padding=1)
-        self.block2 = ResNetBlock(32, 32,  False)
-        self.block3 = ResNetBlock(32, 32,  False)
-        self.block4= ResNetBlock(32, 32, False)
-        self.block5= ResNetBlock(32, 32, False)
-        self.block6 = ResNetBlock(32, 32, False)
-        self.block7 = ResNetBlock(32, 32, False)
-        self.block8 = ResNetBlock(32, 32, False)
-        self.block9 = ResNetBlock(32, 32, False)
-        self.lrelu = nn.LeakyReLU(0.01)
-        self.bn = nn.BatchNorm2d(32)
-        self.dropout = nn.Dropout(0.5)
-        self.logsoftmax = nn.LogSoftmax(dim=1)
-        self.fc1 = nn.Linear(32*10, 128)
-        self.fc2 = nn.Linear(128, 2)
-    
-    def forward(self, x):
-        batch_size = x.size(0)
-        x = x.unsqueeze(dim=1)
-        out = self.conv1(x)
-        out = self.block1(out)
-        out = self.block2(out)
-        out = self.block3(out)
-        out = self.mp(out)
-        out = self.block4(out)
-        out = self.block5(out)
-        out = self.block6(out)
-        out = self.mp(out)
-        out = self.block7(out)
-        out = self.block8(out)
-        out = self.block9(out)
-        out = self.bn(out)
-        out = self.lrelu(out)
-        out = self.mp(out)
-        out = out.view(batch_size, -1)
-        out = self.dropout(out)
-        out = self.fc1(out)
-        out = self.lrelu(out)
-        out = self.fc2(out)
-        out = self.logsoftmax(out)
-        return out
+
         
 def evaluate_accuracy(data_loader, model, device):
     num_correct = 0.0
@@ -180,6 +106,13 @@ def train_epoch(data_loader, model, lr, device):
     train_accuracy = (num_correct/num_total)*100
     return running_loss, train_accuracy
 
+def get_log_spectrum(x):
+    s = librosa.core.stft(x, n_fft=2048, win_length=2048, hop_length=512)
+    a = np.abs(s)**2
+    #melspect = librosa.feature.melspectrogram(S=a)
+    feat = librosa.power_to_db(a)
+    return feat
+
 def compute_mfcc_feats(x):
     mfcc = librosa.feature.mfcc(x, sr=16000, n_mfcc=24)
     delta = librosa.feature.delta(mfcc)
@@ -196,28 +129,40 @@ if __name__ == '__main__':
     parser.add_argument('--num_epochs', type=int, default=100)
     parser.add_argument('--lr', type=float, default=0.0001)
     parser.add_argument('--comment', type=str, default=None, help='Comment to describe the saved mdoel')
-
+    parser.add_argument('--track', type=str, default='logical')
+    parser.add_argument('--features', type=str, default='spect')
     if not os.path.exists('models'):
         os.mkdir('models')
     args = parser.parse_args()
-    model_tag = 'model_{}_{}_{}'.format(args.num_epochs, args.batch_size, args.lr)
+    track = args.track
+    assert args.features in ['mfcc', 'spect'], 'Not supported feature'
+    model_tag = 'model_{}_{}_{}_{}'.format(track, args.features, args.num_epochs, args.batch_size, args.lr)
     if args.comment:
         model_tag = model_tag + '_{}'.format(args.comment)
     model_save_path = os.path.join('models', model_tag)
-
+    assert track in ['logical' , 'physical'], 'Invalid track given'
+    is_logical = (track == 'logical')
     if not os.path.exists(model_save_path):
         os.mkdir(model_save_path)
-    feature_transform = transforms.Compose([
+
+    if args.features == 'mfcc':
+        feature_fn = compute_mfcc_feats
+        model_cls = MFCCModel
+    elif args.features == 'spect':
+        feature_fn = get_log_spectrum
+        model_cls = SpectrogramModel
+    transforms = transforms.Compose([
         lambda x: pad(x),
         lambda x: librosa.util.normalize(x),
-        lambda x: compute_mfcc_feats(x),
-        #lambda x: librosa.feature.chroma_cqt(x, sr=16000, n_chroma=20),
+        lambda x: feature_fn(x),
         lambda x: Tensor(x)
         ])
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    dev_set = data_utils.ASVDataset(is_train=False, transform=feature_transform)
+    dev_set = data_utils.ASVDataset(is_train=False, is_logical=is_logical,
+        transform=transforms,
+        feature_name=args.features)
     dev_loader = DataLoader(dev_set, batch_size=args.batch_size, shuffle=True)
-    model = ConvModel().to(device)
+    model = model_cls().to(device)
     print(args)
 
     if args.model_path:
@@ -230,7 +175,9 @@ if __name__ == '__main__':
         produce_evaluation_file(dev_set, model, device, args.eval_output)
         sys.exit(0)
 
-    train_set = data_utils.ASVDataset(is_train=True, transform=feature_transform)
+    train_set = data_utils.ASVDataset(is_train=True, is_logical=is_logical, transform=transforms,
+                feature_name=args.features)
+
     train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
     num_epochs = args.num_epochs
     writer = SummaryWriter('logs/{}'.format(model_tag))
